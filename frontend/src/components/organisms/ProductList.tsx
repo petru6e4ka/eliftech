04df2e1940feef4a3e74ac24157495a2db589670
anastasiia.vorobiova <anastasiiavorobiova2403@gmail.com>
2023-05-29 13:47:41 +0300

    fix/frontend/layout/productlist

--- frontend/src/components/organisms/ProductList.tsx
+++ frontend/src/components/organisms/ProductList.tsx
@@ -4,31 +4,31 @@ import { ProductCard } from "./ProductCard";
 export const ProductList = () => {
   return (
     <Grid container spacing={2} sx={{ p: 3 }}>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
-      <Grid item xs={12} md={6} lg={4} xl={2}>
+      <Grid item xs={12} md={4} lg={3} xl={2}>
         <ProductCard />
       </Grid>
     </Grid>
