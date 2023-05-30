295e76d669aaa6965c87aaae6bbaf48b297973c4
Anastasiia Vorobiova <51080393+petru6e4ka@users.noreply.github.com>
2023-05-31 00:59:36 +0300

    fix/frontend/css-index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store";
-import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
